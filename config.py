3cd3d1b22714e374338308f912475970f6764fa5
mithunsmenon <mithunsmenon@gmail.com>
2018-08-23 23:08:26 +0530

    CleanUpv2

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 'Token Here'
+TOKEN = 'NDYzMzg3MjEyNDcwNTUwNTI4.Dlk9hA.mfqbBGKO4MMut1FdsSZiclHSlb4'
 
 link = 'https://discordapp.com/oauth2/authorize?client_id=463387212470550528&permissions=1475472631&scope=bot'
