f869e748cd077fdad4b36aa74dc1f037aba41de1
iCodeLife <mithunsmenon@gmail.com>
2018-08-31 21:55:07 +0530

    bot.py:Added Comments

--- bot.py
+++ bot.py
@@ -4,14 +4,16 @@ import time
 from config import TOKEN, link
 from discord.ext import commands
 
- 
+ #prefix
 client = commands.Bot(command_prefix = '/')
 
+#version
 __version__ = '1.0'
+
 
 extensions = ['commands.ping','commands.purge','commands.invite','commands.echo']
 
-
+#On Ready Event
 @client.event
 async def on_ready():
    
@@ -24,8 +26,8 @@ async def on_ready():
     print("Bot started successfully.")
     print("Ctrl+C To Quit")
 
-
-if __name__ == '__main__':
+#module of loading cogs
+    if __name__ == '__main__':
     for extension in extensions:
         try:
             client.load_extension(extension)
@@ -39,6 +41,7 @@ async def bot_quit():
     await client.say("Shutting down...\n\U0001f44b")
     await client.logout()
 
+#bot info
 @client.command(name="info")
 async def bot_info():
     """Display information about the bot."""
